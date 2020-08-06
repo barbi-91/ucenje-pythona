6e6497dff36061c75978d6dacb56059b01be690c
Ivan Golović <igolovic@gmail.com>
2020-08-06 20:14:19 +0200

    promjene vjezbi

---
+++ vjezbe 6.2/6.2.1.py
@@ -0,0 +1,3 @@
+import math
+rezultat=math.sin(2)+math.cos(1)
+print(rezultat)
---
+++ vjezbe 6.2/6.2.2.py
@@ -0,0 +1,6 @@
+import math
+
+a=int(input("unei broj koji zelite korjenovati:"))
+
+b=math.sqrt(a)
+print("korjen od",a ,"je",b)
