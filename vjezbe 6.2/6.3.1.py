368fe41c7cd386b704a41a80aaf82ea9f7c9a021
Ivan Golović <igolovic@gmail.com>
2020-08-08 14:07:02 +0200

    nove vjezbe

--- vjezbe 6.2/6.3.1.py
+++ vjezbe 6.2/6.3.1.py
@@ -2,8 +2,13 @@
 # u modlima ili ti ga u paketima se nalaze osim funkija i konstatne , 
 #a konstatne su npr math.pi...e, tau, inf, nan
 
-#NAJVALJIVANJE KORISTENJA MODULA POMOCU NAREDBE IMPORT: DALI JE POTREBNO PRILIKOM POZIVANJA FUNCIJE NAZNACITI U KOJEM JE MODELU POZIVAJUCA FUNKCIJA
-#da potrebno je prilikom pozivanje funkcije napisati i ime paketa u kkojem se pozivajuca funkcija nalazi?
+##NAJVALJIVANJE KORISTENJA MODULA POMOCU 
+# NAREDBE IMPORT: DALI JE POTREBNO PRILIKOM
+#  POZIVANJA FUNCIJE NAZNACITI U KOJEM JE 
+# MODELU POZIVAJUCA FUNKCIJA?
+##da potrebno je prilikom pozivanje funkcije 
+# napisati i ime paketa u kkojem se pozivajuca 
+# funkcija nalazi?
 #NPR: IMPORT MATH
 #import math
 #c=math.floor(a)
