c962c53cfb4b52c3068ab690bb341d0c1de52dad
Barbara Erdec <barbara.erdec@gmail.com>
2020-08-12 10:59:59 +0200

    mkk

---
+++ vjezbee 9.4.4/9.4.4.1.py
@@ -0,0 +1,5 @@
+nTerac=("a","e", "i", "o", "u")
+print(nTerac[0:3])
+
+
+#printanje prva tri samoglasnika unutar tuple (nTeraca)
---
+++ vjezbee 9.4.4/9.4.4.2.py
@@ -0,0 +1,7 @@
+nTerac=("a","e", "i", "o", "u")
+print(nTerac)
+
+#nTerac[3]=3 #nemogucnost brisanja nteraca
+
+del nTerac
+print(nTerac)
