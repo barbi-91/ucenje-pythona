79be7a037ee55efea66c2d265e74487cf9c55992
Ivan Golović <igolovic@gmail.com>
2020-09-23 11:06:09 +0200

    jjj

--- zavrsneVjezbepolaznik/1.zad.py
+++ zavrsneVjezbepolaznik/1.zad.py
@@ -22,18 +22,37 @@
 # brojac+=1
 
 # print (suma, "suma")
+#************************************************
+# suma=0
+# a=int(input("unesi neki broj"))
+# suma=suma+a
+# print( "unijeli ste a:",a)
+# print("suma je sada:", suma)
+# while True:
+#         b=a
+#         a=int(input("unesi broj:"))
+#         if a<=b:
+#                 break
+#         suma=suma+a
+#         b=a
+#         print(suma, "suma trenutna")
+# print("suma svih vrijednosti je:",suma, 
+#  "uneseni broj se ne racuna jel je ponovljen i program je stao s radom")
+#**************************************************
 
+lista=[]
+trenutni=0
 suma=0
-a=int(input("unesi neki broj"))
-suma=suma+a
-print( "unijeli ste a:",a)
-print("suma je sada:", suma)
 while True:
-        b=a
-        a=int(input("unesi broj:"))
-        if a<=b:
+        n=int(input("unesi broj"))
+        if n==5 and trenutni>n:
+                print("upisani broj je prekidni ili je manji od prethodnog i program se prekida")
                 break
-        suma=suma+a
-        b=a
-        print(suma, "suma trenutna")
-print("suma svih vrijednosti je:",suma,  "uneseni broj se ne racuna jel je ponovljen i program je stao s radom")
+        
+        else:
+                print("broj je veci od prethodnog i broj nije jednak prekidnom")
+                lista.append(n)
+                suma=suma+n
+                trenutni=n
+print("lista trenutno upisanih brojeva", lista)
+print( "suma trenutno upisanih brojeva je", suma)
