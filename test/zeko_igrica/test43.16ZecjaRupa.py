ddd403771809e8ebae024a93f81a20cfa94210b9
barbi-91 <barbara.erdec@gmail.com>
2020-12-11 18:25:11 +0100

    igrica promjene

--- test/zeko_igrica/test43.16ZecjaRupa.py
+++ test/zeko_igrica/test43.16ZecjaRupa.py
@@ -1,13 +1,20 @@
-from test import class Zivotinje()
+from test43.15Zivotinje import Zivotinje
+
 class ZecjaRupa(Scene):
     def enter(self):
         print(dedent("""
             Kako bi se maleni zeko probudio iz najljpseg ziskog sna i shvatio da je
             doslo proljece potrenbno je pogoditi pravilnu sifru na vratima.
+            Zeko je znao da ce usnuti te je zatvorio vrata kako mu ne bi dosla k
+            akva ljut zivotinja dok je on u najljepsem snu, no Zeko nije razmisljao
+            da je vec prije njega netko dr posao na san te je sada on u opasnosti.
+            Potrebno ga je probuditi prije no sto se probudi druga zivtinja koja ce
+            ga pojesti za dorucak!
             """))
 
         vrata = 2003
-        print("Broj koji otvara vrata je cetveroznamen broj i predstavlja datum dolaska proljeca(dan i mjesec)")
+        print("""Broj koji otvara vrata je cetveroznamen broj i predstavlja
+        datum dolaska proljeca(dan i mjesec)""")
 
         pogodak = input ("pogodi sifru >>> ")
 
@@ -18,7 +25,8 @@ class ZecjaRupa(Scene):
             pogodak = input("pogodi: >>> ")
 
         if pogodak == vrata:
-            print("Bili ste tik do pobjede sa budenjem zeke. Pojeo vas je ljuti:")
+            print("""Bili ste tik do pobjede. Na zalost Zeko je izvukao deblji kraj 
+            te ga je pojeo ljuti:""")
             ljuti= Zivotinje()
             ljuti.enter()
             return 'smrt'
