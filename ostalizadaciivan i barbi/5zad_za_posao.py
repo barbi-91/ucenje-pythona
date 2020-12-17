1ada08e56550f951f18c78b5aeeb26364c00ac99
barbi-91 <barbara.erdec@gmail.com>
2020-12-17 18:00:57 +0100

    s

--- ostalizadaciivan i barbi/5zad_za_posao.py
+++ ostalizadaciivan i barbi/5zad_za_posao.py
@@ -1,5 +1,5 @@
-# Napisi metodu string solution(int N) koja vraća string 
-# duljine N takav da je u njemu uvijek neparni broj znakova, 
+# Napisi metodu string solution(int N) koja vraća string
+# duljine N takav da je u njemu uvijek neparni broj znakova,
 # string smije sadržavati samo znakove [a, b, c, d], npr.:
 #da se neparni broj puta ponavllja isto slovo uu jednoj rijeci!
 
@@ -11,42 +11,36 @@
 # 		N = 6, string je "aaabbb" ili "aaaaab"...
 
 import random
-N = int(input("unesi broj duljine stringa:"))  
-def ispis_koda(N):
+N = int(input("unesi broj duljine stringa:"))
+def ispis_koda():
     lista = []
-    
+
 for element in range(1, N+1):
     x = random.randrange(1,5)
 
     if x == 1:
-        x = a
+        x = "a"
     if x == 2:
-        x = b
+        x = "b"
     if x == 3:
-        x = c
+        x = "c"
     if x == 4:
-        x = d
+        x = "d"
     lista.append(x)
 
     for slovo in lista:
-        if slovo == a:
-            tmp = a
-            a = tmp + 1
-        if slovo == b:
-            tmp = b
-            b = tmp + 1
-        if slovo == c:
-            tmp = c
-            c = tmp + 1
-        if slovo == d:
-            tmp = d
-            d = tmp + 1
-    while a % 2 == 0 and b % 2 == 0 and c % 2 == 0 and d % 2 == 0:
+        if slovo == "a":
+            tmp = "a"
+            "a" = tmp + 1
+        if slovo == "b":
+            tmp = "b"
+            "b" = tmp + 1
+        if slovo == "c":
+            tmp = "c"
+            "c" = tmp + 1
+        if slovo == "d":
+            tmp = "d"
+            "d" = tmp + 1
+    while "a" % 2 == 0 and "b" % 2 == 0 and "c" % 2 == 0 and "d" % 2 == 0:
         ispis_koda(N)
-ispis_koda(N) 
-
-
-
-
-
-
+ispis_koda(N)
